22b78e01e85d7f7b7c631911cc22c0f6bff3fc4a
Noy JS <noy.agasi.na@gmail.com>
2025-07-14 04:36:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-base: '/locksmith-plus/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+export default defineConfig(({ mode, command }) => ({
+  base: (command === 'build' && process.env.GITHUB_PAGES === 'true') ? '/locksmith-plus/' : '/',
+  server: {
+    host: "::",
+    port: 8080,
   },
-});
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
